daeda887ab8c5b79ef0d80adb9f27cd8e3eda7e6
Phil Linnell <phil@ustwo.com>
2015-03-25 11:59:38 +0000

    Initial commit with gulpfile and stylus

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Choam
+
+## Steps to create:
+
+Helpful resource: http://travismaynard.com/writing/getting-started-with-gulp
+
+  npm init
+  npm install --save-dev gulp
+  npm install gulp-jshint gulp-stylus gulp-concat gulp-uglify gulp-rename --save-dev
+  npm install nib --save-dev
+  touch gulpfile.js
+
+-----------
+
+  git init
---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var stylus = require('gulp-stylus');
+var nib    = require('nib');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('src/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Compile Our Stylus
+gulp.task('css', function() {
+    return gulp.src('src/stylesheets/*.styl')
+        .pipe(stylus({ use: nib() }))
+        .pipe(gulp.dest('build/css'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('src/js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('src/js/*.js', ['lint', 'scripts']);
+    gulp.watch('src/stylesheets/*.styl', ['css']);
+});
+
+// Default Task
+gulp.task('default', ['lint', 'css', 'scripts', 'watch']);
