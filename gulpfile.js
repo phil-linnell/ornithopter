c698f5cbf141fcdf2e12e8a42597aa3e121a3a1d
Phil Linnell <phil@ustwo.com>
2015-03-30 09:47:17 +0100

    removed express stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,86 @@
-// Include gulp
 var gulp = require('gulp');
 
-// Include Our Plugins
-var server = require('gulp-express');
-var jshint = require('gulp-jshint');
-var stylus = require('gulp-stylus');
-var nib    = require('nib');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var livereload = require('gulp-livereload');
+var stylus      = require('gulp-stylus');
+var nib         = require('nib');
 
-// Lint Task
-gulp.task('lint', function() {
-    return gulp.src('src/js/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
+var concat      = require('gulp-concat');
+var uglify      = require('gulp-uglify');
+var rename      = require('gulp-rename');
+
+var handlebars 	= require('gulp-compile-handlebars');
+
+var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
+
+
+
+gulp.task('browser-sync', function() {
+  browserSync.init( {
+		server: {
+			baseDir: "./build"
+		},
+    port: "10191", // Hey that's the year Dune is set!
+    open: false
+  });
 });
 
-// Compile Our Stylus
+
+// CSS
 gulp.task('css', function() {
-    return gulp.src('src/stylesheets/style.styl')
-        .pipe(stylus({ use: nib() }))
-        .pipe(gulp.dest('public/stylesheets'));
+  return gulp.src('src/stylesheets/style.styl')
+    .pipe(stylus({ use: nib() }))
+    .pipe(gulp.dest('build/stylesheets'))
+    .pipe(reload({stream: true}));
 });
 
-// Concatenate & Minify JS
+// JS
 gulp.task('scripts', function() {
-    return gulp.src('src/js/*.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('public/js'))
-        .pipe(rename('all.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('public/js'));
+  return gulp.src('src/js/*.js')
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('build/js'))
+    .pipe(rename('all.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('build/js'))
+    .pipe(reload({stream:true}));
 });
 
-// Start the server
-gulp.task('server', function() {
-
-  server.run(['./bin/www'], []);
-
-  livereload.listen();
-
-  gulp.watch(['./views/**/*.hbs'], function(event){
-    console.log('hbs changed - do livereload');
-    livereload.reload();
-  });
-
-  gulp.watch('src/stylesheets/*.styl', ['css']);
-
-  gulp.watch(['./public/stylesheets/**/*.css'], function(event){
-      console.log('css changed - do livereload');
-      livereload.reload();
-  });
-
-  gulp.watch('src/js/*.js', ['lint', 'scripts']);
-
-  gulp.watch(['./app.js', './routes/**/*.js'], function(event){
-    server.run(['./bin/www'], []);
-  });
-
+gulp.task('vendor', function() {
+	return gulp.src('src/js/vendor/*.js')
+		.pipe(gulp.dest('build/js/vendor'));
 });
 
-// Default Task
-gulp.task('default', ['server', 'lint', 'css', 'scripts']);
+
+// Templates
+gulp.task('templates', function() {
+	var templateData = {
+	  title: 'Fremen'
+	};
+	var options = {
+	  ignorePartials: true,
+	  batch: ['./src/views'],
+	  helpers: {
+	    capitals: function(str){
+	      return str.toUpperCase();
+	    }
+	  }
+	};
+	return gulp.src('src/views/*.html')
+		.pipe(handlebars(templateData, options))
+		.pipe(gulp.dest('build/'))
+		.pipe(reload({stream:true}));
+});
+
+
+
+// Initial build
+gulp.task('build', ['css', 'scripts', 'vendor', 'templates']);
+
+// Watch
+gulp.task('watch', function() {
+	gulp.watch('src/stylesheets/**/*.styl', ['css']);
+  gulp.watch('src/js/**/*.js', ['scripts']);
+  gulp.watch('src/views/**/*.html', ['templates']);
+});
+
+
+gulp.task('default', ['build', 'browser-sync', 'watch']);
