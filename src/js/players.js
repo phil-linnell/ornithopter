6adbfc4d11a8d068a48d2f790eff4c0ff311aae2
Phil Linnell <phil@ustwo.com>
2015-03-31 10:19:22 +0100

    positions and colours

--- src/js/players.js
+++ src/js/players.js
@@ -2,47 +2,70 @@
 
 $(document).ready(function() {
 
+  // Some settings
+
   var canvas  = 220;
-
-  var svg = d3.select("#players").attr("width", canvas).attr("height", canvas);
-
-
-  var startingcolour = {
-    yellow: "#e7e00f",
-    red: "#e71f0f",
-    white: "white",
-    blue: "#1128d4",
-    green: "#268b13",
-    black: "black",
-    pink: "#de77cc",
-    orange: "#e9980c",
-    purple: "#890ce9"
-  };
-
-
   var size    = 40;
-  var margin  = 20;
-  var amount  = 9;
   var rows    = 3;
   var cols    = 3;
 
 
-  function players() {
-    for (r = 0; r < rows; r++) {
-      for (c = 0; c < cols; c++) {
-        var player = svg.append("circle")
-          .attr("r", (size * 0.5))
-          .attr("cx", ((canvas / cols)) * (c + 1) - (canvas / cols * 0.5))
-          .attr("cy", ((canvas / rows)) * (r + 1) - (canvas / cols * 0.5))
-          .style("fill", "red");
-        for (var x in startingcolour) {
-          player.style("fill", x);
-        }
-      }
+  // Intialise d3
+
+  var svg = d3.select("#players").attr("width", canvas).attr("height", canvas);
+
+
+
+  // Create circles and assign colours
+
+  var playerColours = [
+    {color: 'yellow', value: '#e7e00f'},
+    {color: 'red', value: '#e71f0f'},
+    {color: 'white', value: 'white'},
+    {color: 'blue', value: '#1128d4'},
+    {color: 'green', value: '#268b13'},
+    {color: 'black', value: 'black'},
+    {color: 'pink', value: '#de77cc'},
+    {color: 'orange', value: '#e9980c'},
+    {color: 'purple', value: '#890ce9'}
+  ];
+
+  var set = svg.selectAll("circle")
+            .data(playerColours)
+            .enter()
+            .append("circle")
+            .attr("r", (size * 0.5))
+            .attr("class", function(d) {
+              return d.color;
+            })
+            .style("fill", function(d) {
+              return d.value;
+            });
+
+
+
+  // Calculate positions
+
+  var positions = [];
+  for (r = 0; r < rows; r++) {
+    for (c = 0; c < cols; c++) {
+      positions.push({row: r, col: c});
     }
   }
 
-  players();
+  set.data(positions)
+      .attr("cx", function(d) {
+        return (canvas / rows) * (d.row + 1) - (canvas / rows * 0.5);
+      })
+      .attr("cy", function(d) {
+        return (canvas / cols) * (d.col + 1) - (canvas / cols * 0.5);
+      });
+
+
+
+  $('circle').on('click', function() {
+    // $(this).attr("r", size);
+  });
 
 
 });
