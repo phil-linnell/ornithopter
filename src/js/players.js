81bd870f96712bd7a87aad26f431b76e80779c3b
Phil Linnell <phil@ustwo.com>
2015-04-01 11:10:26 +0100

    circle positions

--- src/js/players.js
+++ src/js/players.js
@@ -4,10 +4,8 @@ $(document).ready(function() {
 
   // Some settings
 
+  var size    = 20;
   var canvas  = 220;
-  var size    = 40;
-  var rows    = 3;
-  var cols    = 3;
 
 
   // Intialise d3
@@ -26,16 +24,16 @@ $(document).ready(function() {
     {color: 'green', value: '#268b13'},
     {color: 'black', value: 'black'},
     {color: 'pink', value: '#de77cc'},
-    {color: 'orange', value: '#e9980c'},
-    {color: 'purple', value: '#890ce9'}
+    {color: 'purple', value: '#890ce9'},
+    {color: 'orange', value: '#e9980c'}
   ];
 
-  var set = svg.selectAll("circle")
+  var player = svg.selectAll("circle")
             .data(playerColours)
             .enter()
             .append("circle")
-            .attr("r", (size * 0.5))
-            .attr("class", function(d) {
+            .attr("r", size)
+            .attr("data-name", function(d) {
               return d.color;
             })
             .style("fill", function(d) {
@@ -44,16 +42,21 @@ $(document).ready(function() {
 
 
 
-  // Calculate positions
 
+  // Rows and Columns
+
+  var rows    = 3;
+  var cols    = 3;
   var positions = [];
+
   for (r = 0; r < rows; r++) {
     for (c = 0; c < cols; c++) {
       positions.push({row: r, col: c});
     }
   }
 
-  set.data(positions)
+  player.data(positions)
+  .transition()
       .attr("cx", function(d) {
         return (canvas / rows) * (d.row + 1) - (canvas / rows * 0.5);
       })
@@ -63,9 +66,63 @@ $(document).ready(function() {
 
 
 
-  $('circle').on('click', function() {
-    // $(this).attr("r", size);
+
+  // Select players
+
+  d3.selectAll("circle").on("click", function () {
+    var activeClass = "active";
+    var alreadyIsActive = d3.select(this).classed(activeClass);
+    // svg.selectAll(".reports-chart__bar")
+    //   .classed(activeClass, false);
+    d3.select(this).classed(activeClass, !alreadyIsActive);
   });
+
+
+
+  // Spin
+
+  var selected = [];
+
+  $('.submit').on("click", function() {
+
+    $('.wrapper').addClass('animate');
+
+    $('circle.active').each(function() {
+      selected.push($(this).data('name'));
+    });
+
+    $('circle:not(.active)').detach();
+    var amount = selected.length;
+
+    var result = selected[Math.floor(Math.random() * selected.length)];
+    console.log(result);
+
+    var canvas = (amount * 10) + 30;
+    d3.select("#players").transition().attr("width", canvas).attr("height", canvas);
+
+    var newPositions = [];
+    var cDiam = canvas - size;
+    var cRad  = cDiam / 2;
+    var offset = (canvas - cDiam) / 2;
+    var cAngle = (2 * Math.PI) / amount;
+
+    for (p = 0; p < amount; p++) {
+      var x = offset + cRad + cRad * Math.cos(cAngle * (p + 1));
+      var y = offset + cRad + cRad * Math.sin(cAngle * (p + 1));
+      newPositions.push({xCoor: x, yCoor: y});
+    }
+
+    svg.selectAll('circle').data(newPositions)
+        .transition()
+        .attr("cx", function(d) {
+          return d.xCoor;
+        })
+        .attr("cy", function(d) {
+          return d.yCoor;
+        });
+
+  });
+
 
 
 });
