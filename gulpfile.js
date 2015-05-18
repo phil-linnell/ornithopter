e42a95209800d8836b9efd17d64385b58293096d
Phil Linnell <phil@ustwo.com>
2015-05-18 09:55:01 +0100

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var gulp = require('gulp');
 var stylus      = require('gulp-stylus');
 var nib         = require('nib');
 
+var handlebars 	= require('gulp-compile-handlebars');
+
 var browserSync = require('browser-sync');
 var reload      = browserSync.reload;
 
@@ -21,14 +23,34 @@ gulp.task('browser-sync', function() {
 // CSS
 gulp.task('css', function() {
   return gulp.src('src/stylesheets/style.styl')
+    .pipe(stylus({use: nib()}))
     .pipe(gulp.dest('build/stylesheets'))
     .pipe(reload({stream: true}));
 });
 
+// Templates
+gulp.task('templates', function() {
+	var templateData = {
+	  title: 'Ornithopter'
+	};
+	var options = {
+	  ignorePartials: true,
+	  batch: ['./src/views'],
+	  helpers: {
+	    capitals: function(str){
+	      return str.toUpperCase();
+	    }
+	  }
+	};
+	return gulp.src('src/views/*.html')
+		.pipe(handlebars(templateData, options))
+		.pipe(gulp.dest('build/'))
+		.pipe(reload({stream:true}));
+});
 
 
 // Initial/production build
-gulp.task('build', ['css']);
+gulp.task('build', ['css', 'templates']);
 
 // Watch
 gulp.task('watch', function() {
