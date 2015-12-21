4b296ee4bd565c7e281d88b149f4d5b86f3caef7
Phil Linnell <phil@ustwo.com>
2015-12-21 16:05:46 +0000

    Tokeniser and tests

--- tests/ornithopter_test.js
+++ tests/ornithopter_test.js
@@ -1,11 +1,30 @@
 import {expect} from "chai";
-import O from "ornithopter";
-import {processColor} from "ornithopter";
+import {processColor, tokeniser} from "ornithopter";
 
 describe("ornithopter", () => {
   describe(".processColor()", () => {
-    it("noop", () => {
-      expect(processColor(['red', 'green'])).to.deep.equal(['red', 'green']);
+    it("should return keyframes", () => {
+      var actual = processColor(['red', 'green']);
+      var expected = [{step: 0, properties: [{name: 'color', value: 'red'}]},
+                      {step: 100, properties: [{name: 'color', value: 'green'}]}];
+      expect(actual).to.deep.equal(expected);
+    });
+    it("should return keyframes", () => {
+      var actual = processColor(['yellow', 'blue']);
+      var expected = [{step: 0, properties: [{name: 'color', value: 'yellow'}]},
+                      {step: 100, properties: [{name: 'color', value: 'blue'}]}];
+      expect(actual).to.deep.equal(expected);
+    });
+    // it("should return keyframes that includes a delay", () => {
+    //   var actual = processColor(['red', 'green', '1s', '1s']);
+    //   var expected = [{step: 50, properties: [{name: 'color', value: 'red'}]},
+    //                   {step: 100, properties: [{name: 'color', value: 'green'}]}];
+    //   expect(actual).to.deep.equal(expected);
+    // });
+  });
+  describe(".tokeniser()", () => {
+    it("should return an array", () => {
+      expect(tokeniser("red green")).to.deep.equal(['red', 'green']);
     });
   });
 });
