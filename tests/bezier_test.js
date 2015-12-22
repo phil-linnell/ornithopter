23cac06214dedcae03540b180726e48928696974
Arnau Siches <asiches@gmail.com>
2015-12-22 12:57:39 +0000

    Fix b function order

--- tests/bezier_test.js
+++ tests/bezier_test.js
@@ -4,60 +4,60 @@ import {point, linear, quadratic, cubic} from "bezier";
 describe("bezier", () => {
   describe(".linear()", () => {
     it("should return a point at 0", () => {
-      expect(linear(0, point(50, 50),
-                       point(300, 300))).to.deep.equal(point(300, 300));
+      expect(linear(0, point(0, 0),
+                       point(1, 1))).to.deep.equal(point(0, 0));
     });
 
     it("should return a point at 0.5", () => {
-      expect(linear(0.5, point(50, 50),
-                         point(300, 300))).to.deep.equal(point(175, 175));
+      expect(linear(0.5, point(0, 0),
+                         point(1, 1))).to.deep.equal(point(0.5, 0.5));
     });
     it("should return a point at 1", () => {
-      expect(linear(1, point(50, 50),
-                       point(300, 300))).to.deep.equal(point(50, 50));
+      expect(linear(1, point(0, 0),
+                       point(1, 1))).to.deep.equal(point(1, 1));
     });
   });
 
   describe(".quadratic()", () => {
     it("should return a point at 0", () => {
-      expect(quadratic(0, point(50, 50),
-                          point(50, 300),
-                          point(300, 300))).to.deep.equal(point(300, 300));
+      expect(quadratic(0, point(0, 0),
+                          point(0, 1),
+                          point(1, 1))).to.deep.equal(point(0, 0));
     });
 
     it("should return a point at 0.5", () => {
-      expect(quadratic(0.5, point(50, 50),
-                            point(50, 300),
-                            point(300, 300))).to.deep.equal(point(112.5, 237.5));
+      expect(quadratic(0.5, point(0, 0),
+                            point(0, 1),
+                            point(1, 1))).to.deep.equal(point(0.25, 0.75));
     });
 
     it("should return a point at 1", () => {
-      expect(quadratic(1, point(50, 50),
-                          point(50, 300),
-                          point(300, 300))).to.deep.equal(point(50, 50));
+      expect(quadratic(1, point(0, 0),
+                          point(0, 1),
+                          point(1, 1))).to.deep.equal(point(1, 1));
     });
   });
 
   describe(".cubic()", () => {
     it("should return a point at 0", () => {
-      expect(cubic(0, point(50, 50),
-                      point(50, 300),
-                      point(300, 50),
-                      point(300, 300))).to.deep.equal(point(300, 300));
+      expect(cubic(0, point(0, 0),
+                      point(0, 1),
+                      point(1, 0),
+                      point(1, 1))).to.deep.equal(point(0, 0));
     });
 
     it("should return a point at 0.5", () => {
-      expect(cubic(0.5, point(50, 50),
-                        point(50, 300),
-                        point(300, 50),
-                        point(300, 300))).to.deep.equal(point(175, 175));
+      expect(cubic(0.5, point(0, 0),
+                        point(0, 1),
+                        point(1, 0),
+                        point(1, 1))).to.deep.equal(point(0.5, 0.5));
     });
 
     it("should return a point at 1", () => {
-      expect(cubic(1, point(50, 50),
-                      point(50, 300),
-                      point(300, 50),
-                      point(300, 300))).to.deep.equal(point(50, 50));
+      expect(cubic(1, point(0, 0),
+                      point(0, 1),
+                      point(1, 0),
+                      point(1, 1))).to.deep.equal(point(1, 1));
     });
   });
 });
