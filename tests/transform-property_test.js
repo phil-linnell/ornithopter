8c26a899e3fd1a9da66fe78735ef83a43fb4410b
Phil Linnell <phil@ustwo.com>
2016-02-23 13:23:44 +0000

    Unify process function

--- tests/transform-property_test.js
+++ tests/transform-property_test.js
@@ -1,22 +1,22 @@
 import {expect} from "chai";
-import {processTransform} from "ornithopter";
+import {process} from "ornithopter";
 
 describe("ornithopter", () => {
-  describe(".processTransform()", () => {
+  describe(".process()", () => {
     it("should return two keyframes for transform with translateX", () => {
-      var actual = processTransform('transform', 'translateX', ['100px', '200px']);
+      var actual = process('transform', ['100px', '200px'], 'translateX');
       var expected = [{step: 0, properties: [{property: 'transform', value: 'translateX(100px)'}]},
                       {step: 100, properties: [{property: 'transform', value: 'translateX(200px)'}]}];
       expect(actual).to.deep.equal(expected);
     });
     it("should return two keyframes for transform with rotate", () => {
-      var actual = processTransform('transform', 'rotate', ['0deg', '360deg']);
+      var actual = process('transform', ['0deg', '360deg'], 'rotate');
       var expected = [{step: 0, properties: [{property: 'transform', value: 'rotate(0deg)'}]},
                       {step: 100, properties: [{property: 'transform', value: 'rotate(360deg)'}]}];
       expect(actual).to.deep.equal(expected);
     });
     it("should return three keyframes for transform with translateY", () => {
-      var actual = processTransform('transform', 'translateX', ['0', '100px', '150px']);
+      var actual = process('transform', ['0', '100px', '150px'], 'translateX');
       var expected = [{step: 0, properties: [{property: 'transform', value: 'translateX(0)'}]},
                       {step: 50, properties: [{property: 'transform', value: 'translateX(100px)'}]},
                       {step: 100, properties: [{property: 'transform', value: 'translateX(150px)'}]}];
