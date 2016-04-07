31be8f92ac78b6289c81c0fec2c5cff8b81d7f3f
Phil Linnell <phil@ustwo.com>
2016-04-07 13:52:38 +0100

    changePoint and percentage functions in steps

--- tests/steps_test.js
+++ tests/steps_test.js
@@ -1,5 +1,5 @@
 import {expect} from "chai";
-import {first, next} from "steps";
+import {first, next, changePoint, percentage} from "steps";
 
 describe("steps", () => {
   describe(".first()", () => {
@@ -53,6 +53,41 @@ describe("steps", () => {
                   previous,
                   totalDuration,
                   totalSteps)).to.equal(100);
+    });
+  });
+  describe(".changePoint()", () => {
+    it("should always return 0", () => {
+      expect(changePoint(0, 4, n => n)).to.equal(0);
+    });
+
+    it("should always return 1", () => {
+      expect(changePoint(4, 4, n => n)).to.equal(1);
+    });
+
+    it("should return a change point between 0 and 1", () => {
+      expect(changePoint(1, 4, n => n)).to.equal(0.25);
+    });
+
+    it("should return a change point between 0 and 1", () => {
+      expect(changePoint(3, 7, n => n)).to.equal(0.429);
+    });
+
+    it("should return a change point between 0 and 1", () => {
+      expect(changePoint(5, 11, n => n)).to.equal(0.455);
+    });
+
+    it("should return a change point between 0 and 1", () => {
+      expect(changePoint(4, 9, n => n)).to.equal(0.444);
+    });
+  });
+
+  describe(".percentage()", () => {
+    it("should return the second keyframe with no delay", () => {
+      expect(percentage(0.25, 500, 0)).to.equal(25);
+    });
+
+    it("should return the second keyframe with a .5s delay", () => {
+      expect(percentage(0.25, 500, 500, 1000)).to.equal(62.5);
     });
   });
 });
