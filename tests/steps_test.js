cd74f718592eb243498ea777acd7368a8fa20bfd
Arnau Siches <asiches@gmail.com>
2016-04-07 07:36:35 +0100

    Broken: Add delay to steps.next()

--- tests/steps_test.js
+++ tests/steps_test.js
@@ -1,19 +1,58 @@
 import {expect} from "chai";
-import {initialStepValue, nextStepValue} from "ornithopter";
+import {first, next} from "steps";
 
 describe("steps", () => {
-  describe(".initialStepValue()", () => {
+  describe(".first()", () => {
     it("should return 0 when delay is 0", () => {
-      expect(initialStepValue([1, 0, 'linear'], 1)).to.equal(0);
+      expect(first([1, 0, 'linear'], 1)).to.equal(0);
     });
+
     it("should return 50 when delay is 0.5", () => {
-      expect(initialStepValue([1, 0.5, 'linear'], 1)).to.equal(50);
+      expect(first([1, 0.5, 'linear'], 1)).to.equal(50);
     });
   });
 
-  describe(".nextStepValue()", () => {
+  describe(".next()", () => {
     it("should return the next step from 0", () => {
-      expect(initialStepValue(1, 0, 1, 2)).to.equal(100);
+      const duration = 1;
+      const delay = 0;
+      const previous = 0;
+      const totalDuration = 1;
+      const totalSteps = 2;
+
+      expect(next(duration,
+                  delay,
+                  previous,
+                  totalDuration,
+                  totalSteps)).to.equal(100);
+    });
+
+    it("should return the next step from 50 with no delay", () => {
+      const duration = 1;
+      const delay = 0;
+      const previous = 50;
+      const totalDuration = 1;
+      const totalSteps = 2;
+
+      expect(next(duration,
+                  delay,
+                  previous,
+                  totalDuration,
+                  totalSteps)).to.equal(100);
+    });
+
+    it("should return the next step from 50 with delay", () => {
+      const duration = 0.5;
+      const delay = 0.5;
+      const previous = 50;
+      const totalDuration = 1;
+      const totalSteps = 2;
+
+      expect(next(duration,
+                  delay,
+                  previous,
+                  totalDuration,
+                  totalSteps)).to.equal(100);
     });
   });
 });
