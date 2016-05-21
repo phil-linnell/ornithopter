84dbde115ff185aba4ec421f7cb1297a30d13952
Arnau Siches <asiches@gmail.com>
2016-05-21 16:08:18 +0100

    Fix interpolateValue for Number and Color

--- tests/color_test.js
+++ tests/color_test.js
@@ -1,52 +1,37 @@
-import {expect} from "chai";
-import Color, {isColor, rangeLength} from "utils/color";
-import {keyframeValue, valueByLinearColor} from "steps";
+import { expect } from 'chai';
+import Color, { isColor, rangeLength } from 'utils/color';
 
-describe("color", function () {
-  describe("experiment", function () {
-    it("sanity check", function () {
-      expect(Color("red").rgb()).to.deep.equal({r: 255, g: 0, b: 0});
-      expect(Color("deeppink").rgb()).to.deep.equal({r: 255, g: 20, b: 147});
+describe('color', function () {
+  describe('experiment', function () {
+    it('sanity check', function () {
+      expect(Color('red').rgb()).to.deep.equal({r: 255, g: 0, b: 0});
+      expect(Color('deeppink').rgb()).to.deep.equal({r: 255, g: 20, b: 147});
     });
   });
 
-  describe("Is color?", function () {
-    it("isColor() true", function () {
-      expect(isColor("red")).to.be.true;
-      expect(isColor("deeppink")).to.be.true;
+  describe('Is color?', function () {
+    it('isColor() true', function () {
+      expect(isColor('red')).to.be.true;
+      expect(isColor('deeppink')).to.be.true;
     });
 
-    it("isColor() false", function () {
-      expect(isColor("bronze")).to.be.false;
+    it('isColor() false', function () {
+      expect(isColor('bronze')).to.be.false;
     });
 
-    it("isColor() false", function () {
-      expect(isColor("100px")).to.be.false;
-      expect(isColor("0")).to.be.false;
+    it('isColor() false', function () {
+      expect(isColor('100px')).to.be.false;
+      expect(isColor('0')).to.be.false;
     });
   });
 
-  describe("Range length", function () {
-    it("rangeLength()", function () {
+  describe('Range length', function () {
+    it('rangeLength()', function () {
       expect(rangeLength(255, 0)).to.equal(255);
     });
 
-    it("rangeLength()", function () {
+    it('rangeLength()', function () {
       expect(rangeLength(50, 255)).to.equal(-205);
-    });
-  });
-
-  describe("keyframeValue", function () {
-    it("should return the first value", function () {
-      expect(keyframeValue(0, null, [Color("red"), Color("green")], 4, x => x)).to.deep.equal(Color("red"));
-    });
-
-    it("should return the last value", function () {
-      expect(keyframeValue(4, null, [Color("red"), Color("green")], 4, x => x)).to.deep.equal(Color("green"));
-    });
-
-    it("should return the second value", function () {
-      expect(keyframeValue(1, Color("red"), [Color("red"), Color("green")], 4, valueByLinearColor)).to.deep.equal(Color({r: 204, g: 26, b: 0}));
     });
   });
 });
--- tests/steps_test.js
+++ tests/steps_test.js
@@ -1,20 +1,27 @@
-import {expect} from "chai";
-import {first, next, changePoint, percentage, keyframeValue, valueByLinear, castValues} from "steps";
-import Color from "utils/color";
+import { expect } from 'chai';
+import { first,
+         next,
+         changePoint,
+         percentage,
+         interpolateValue,
+         valueByLinear,
+         valueByLinearColor } from 'steps';
+import Color from 'utils/color';
+import { Number } from 'utils/number';
 
-describe("steps", () => {
-  describe(".first()", () => {
-    it("should return 0 when delay is 0", () => {
+describe('steps', function () {
+  describe('.first()', function () {
+    it('should return 0 when delay is 0', function () {
       expect(first([1, 0, 'linear'], 1)).to.equal(0);
     });
 
-    it("should return 50 when delay is 0.5", () => {
+    it('should return 50 when delay is 0.5', function () {
       expect(first([1, 0.5, 'linear'], 1)).to.equal(50);
     });
   });
 
-  describe(".next()", () => {
-    it("should return the next step from 0", () => {
+  describe('.next()', function () {
+    it('should return the next step from 0', function () {
       const duration = 1;
       const delay = 0;
       const previous = 0;
@@ -28,7 +35,7 @@ describe("steps", () => {
                   totalSteps)).to.equal(100);
     });
 
-    it("should return the next step from 50 with no delay", () => {
+    it('should return the next step from 50 with no delay', function () {
       const duration = 1;
       const delay = 0;
       const previous = 50;
@@ -42,7 +49,7 @@ describe("steps", () => {
                   totalSteps)).to.equal(100);
     });
 
-    it("should return the next step from 50 with delay", () => {
+    it('should return the next step from 50 with delay', function () {
       const duration = 0.5;
       const delay = 0.5;
       const previous = 50;
@@ -56,106 +63,107 @@ describe("steps", () => {
                   totalSteps)).to.equal(100);
     });
   });
-  describe(".changePoint()", () => {
-    it("should always return 0", () => {
+  describe('.changePoint()', function () {
+    it('should always return 0', function () {
       expect(Math.round(changePoint(0, 4, n => n) * 1000) / 1000).to.equal(0);
     });
 
-    it("should always return 1", () => {
+    it('should always return 1', function () {
       expect(Math.round(changePoint(4, 4, n => n) * 1000) / 1000).to.equal(1);
     });
 
-    it("should return a change point between 0 and 1", () => {
+    it('should return a change point between 0 and 1', function () {
       expect(Math.round(changePoint(1, 4, n => n) * 1000) / 1000).to.equal(0.25);
     });
 
-    it("should return a change point between 0 and 1", () => {
+    it('should return a change point between 0 and 1', function () {
       expect(Math.round(changePoint(3, 7, n => n) * 1000) / 1000).to.equal(0.429);
     });
 
-    it("should return a change point between 0 and 1", () => {
+    it('should return a change point between 0 and 1', function () {
       expect(Math.round(changePoint(5, 11, n => n) * 1000) / 1000).to.equal(0.455);
     });
 
-    it("should return a change point between 0 and 1", () => {
+    it('should return a change point between 0 and 1', function () {
       expect(Math.round(changePoint(4, 9, n => n) * 1000) / 1000).to.equal(0.444);
     });
   });
 
-  describe(".percentage()", () => {
-    it("should return the second keyframe with no delay", () => {
+  describe('.percentage()', function () {
+    it('should return the second keyframe with no delay', function () {
       expect(percentage(0.25, 500, 0)).to.equal(25);
     });
 
-    it("should return the second keyframe with a .5s delay", () => {
+    it('should return the second keyframe with a .5s delay', function () {
       expect(percentage(0.25, 500, 500, 1000)).to.equal(62.5);
     });
 
-    it("should return the second keyframe with a .2s delay", () => {
+    it('should return the second keyframe with a .2s delay', function () {
       expect(percentage(0.5, 800, 200, 1000)).to.equal(60);
     });
 
-    it("should return the middle keyframe with a .2s delay and shorter than total duration animation", () => {
+    it('should return the middle keyframe with a .2s delay and shorter than total duration animation', function () {
       expect(percentage(0.5, 200, 200, 1000)).to.equal(30);
     });
 
-    it("should return the first keyframe with a .1s delay and shorter than total duration animation", () => {
+    it('should return the first keyframe with a .1s delay and shorter than total duration animation', function () {
       expect(percentage(0, 160, 100, 640)).to.equal(15.63);
     });
 
-    it("should return the middle keyframe with a .1s delay and shorter than total duration animation", () => {
+    it('should return the middle keyframe with a .1s delay and shorter than total duration animation', function () {
       expect(percentage(0.5, 160, 100, 640)).to.equal(28.13);
     });
 
-    it("should return the penultimate keyframe with a .1s delay and shorter than total duration animation", () => {
+    it('should return the penultimate keyframe with a .1s delay and shorter than total duration animation', function () {
       expect(percentage(0.75, 480, 70, 825)).to.equal(52.12);
     });
 
-    it("should return the penultimate keyframe with a .1s delay and shorter than total duration animation", () => {
+    it('should return the penultimate keyframe with a .1s delay and shorter than total duration animation', function () {
       expect(percentage((1/3), 1000, 0, 1000)).to.equal(33.33);
     });
   });
 
-  describe(".keyframeValue()", () => {
-    it("should return the first value", () => {
-      expect(keyframeValue(0, null, [0, 100], 4, x => x)).to.equal(0);
+  describe('.interpolateValue()', function () {
+    it('should return the first value', function () {
+      expect(interpolateValue(0, null, [Number(0, ''), Number(100, '')], 4, valueByLinear)).to.deep.equal(Number(0, ''));
     });
 
-    it("should return the last value", () => {
-      expect(keyframeValue(4, null, [0, 100], 4, x => x)).to.equal(100);
+    it('should return the last value', function () {
+      expect(interpolateValue(4, null, [Number(0, ''), Number(100, '')], 4, valueByLinear)).to.deep.equal(Number(100, ''));
     });
 
-    it("should return the second value", () => {
-      expect(keyframeValue(1, 0, [0, 100], 4, valueByLinear)).to.equal(20);
-    });
-  });
-
-  describe(".valueByLinear()", () => {
-    it("should return the nth value in a linear timing function", () => {
-      expect(valueByLinear(4, 0, 100)).to.equal(20);
+    it('should return the second value', function () {
+      expect(interpolateValue(1, Number(0, ''), [Number(0, ''), Number(100, '')], 4, valueByLinear)).to.deep.equal(Number(20, ''));
     });
 
-    it("should return the nth value in a linear timing function", () => {
-      expect(valueByLinear(4, 40, 100)).to.equal(60);
+    it("should return the first value", function () {
+      expect(interpolateValue(0, null, [Color("red"), Color("green")], 4, x => x)).to.deep.equal(Color('red'));
     });
 
-    it("should return the nth value in a linear timing function", () => {
-      expect(valueByLinear(4, 14.6, 73)).to.equal(29.2);
+    it("should return the last value", function () {
+      expect(interpolateValue(4, null, [Color("red"), Color("green")], 4, x => x)).to.deep.equal(Color('green'));
     });
 
-    it("should return the nth color value in a linear timing function", () => {
-      expect(valueByLinear(4, Color("red"), Color("pink"))).to.deep.equal("rgb(255, 38, 41)");
+    it("should return the second value", function () {
+      expect(interpolateValue(1, Color("red"), [Color("red"), Color("green")], 4, valueByLinearColor)).to.deep.equal(Color({r: 204, g: 26, b: 0}));
     });
   });
-});
 
+  describe('.valueByLinear()', function () {
+    it('should return the nth value in a linear timing function', function () {
+      expect(valueByLinear(4, Number(0, ''), Number(100, ''))).to.deep.equal(Number(20, ''));
+    });
 
-describe("castValues()", function () {
-  it("should keep numeric values untouched", function () {
-    expect(castValues(['0', '100px'])).to.deep.equal(['0', '100px']);
-  });
+    it('should return the nth value in a linear timing function', function () {
+      expect(valueByLinear(4, Number(40, '%'), Number(100, '%'))).to.deep.equal(Number(60, '%'));
+    });
 
-  it("should cast color values as Color", function () {
-    expect(castValues(['red', '#FF00AA'])).to.deep.equal([Color('red'), Color('#FF00AA')]);
+    it('should return the nth value in a linear timing function', function () {
+      expect(valueByLinear(4, Number(14.6, ''), Number(73, ''))).to.deep.equal(Number(29.2, ''));
+    });
+
+    it('should return the nth color value in a linear timing function', function () {
+      expect(valueByLinearColor(4, Color('red'), Color('pink'))).to.deep.equal(Color().rgb(255, 38, 41));
+    });
   });
 });
