216f680828e393173d3cf595e89f18e0fa2ce3d3
Arnau Siches <asiches@gmail.com>
2016-05-22 09:08:46 +0100

    Rollback to placeholder.reduce

--- tests/multiple-properties_test.js
+++ tests/multiple-properties_test.js
@@ -1,16 +1,29 @@
-import {expect} from "chai";
-import {processMultiple} from "ornithopter";
+import { expect } from 'chai';
+import { processMultiple } from 'ornithopter';
 
-describe("ornithopter", function () {
-  describe(".processMultiple()", function () {
-    it("should return two keyframes for width and transform", function () {
-      const actual = processMultiple([{property: 'color', value: ['red', 'green']},
-                                    {property: 'transform', value: ['0', '100px'], fn: 'translateX'}], '1s');
-      const expected = [{step: 0, properties: [{property: 'color', value: 'red'},
-                                             {property: 'transform', value: 'translateX(0)'}]},
-                      {step: 100, properties: [{property: 'color', value: 'green'},
-                                               {property: 'transform', value: 'translateX(100px)'}]}];
-      expect(actual).to.deep.equal(expected);
+describe('ornithopter', function () {
+  describe('.processMultiple()', function () {
+    it('should return two keyframes for width and transform', function () {
+      const properties = [
+        { property: 'color', value: ['red', 'green'] },
+        { property: 'transform', value: ['0', '100px'], fn: 'translateX' }
+      ];
+
+      const expected = [{
+        step: 0,
+        properties: [
+          { property: 'color', value: 'red' },
+          { property: 'transform', value: 'translateX(0)' }
+        ]
+      }, {
+        step: 100,
+        properties: [
+          { property: 'color', value: 'green' },
+          { property: 'transform', value: 'translateX(100px)' }
+        ]
+      }];
+
+      expect(processMultiple(properties, '1s')).to.deep.equal(expected);
     });
   });
 });
--- tests/transform-property_test.js
+++ tests/transform-property_test.js
@@ -1,25 +1,47 @@
-import {expect} from "chai";
-import {process} from "ornithopter";
+import { expect } from 'chai';
+import { process } from 'ornithopter';
 
-describe("ornithopter", function () {
-  describe(".process()", function () {
-    it("should return two keyframes for transform with translateX", function () {
-      const actual = process({property: 'transform', value: ['100px', '200px'], fn: 'translateX', totalDuration: '1s'});
-      const expected = [{step: 0, properties: [{property: 'transform', value: 'translateX(100px)'}]},
-                      {step: 100, properties: [{property: 'transform', value: 'translateX(200px)'}]}];
+describe('ornithopter', function () {
+  describe('.process()', function () {
+    it('should return two keyframes for transform with translateX', function () {
+      const actual = process({ property: 'transform', value: ['100px', '200px'], fn: 'translateX', totalDuration: '1s' });
+      const expected = [{
+        step: 0,
+        properties: [{ property: 'transform', value: 'translateX(100px)' }]
+      }, {
+        step: 100,
+        properties: [{ property: 'transform', value: 'translateX(200px)' }]
+      }];
+
       expect(actual).to.deep.equal(expected);
     });
-    it("should return two keyframes for transform with rotate", function () {
-      const actual = process({property: 'transform', value: ['0deg', '360deg'], fn: 'rotate', totalDuration: '1s'});
-      const expected = [{step: 0, properties: [{property: 'transform', value: 'rotate(0deg)'}]},
-                      {step: 100, properties: [{property: 'transform', value: 'rotate(360deg)'}]}];
+
+    it('should return two keyframes for transform with rotate', function () {
+      const actual = process({ property: 'transform', value: ['0deg', '360deg'], fn: 'rotate', totalDuration: '1s' });
+      const expected = [{
+        step: 0,
+        properties: [{property: 'transform', value: 'rotate(0deg)'}]
+      }, {
+        step: 100,
+        properties: [{property: 'transform', value: 'rotate(360deg)'}]
+      }];
+
       expect(actual).to.deep.equal(expected);
     });
-    it("should return three keyframes for transform with translateY", function () {
-      const actual = process({property: 'transform', value: ['0', '100px', '150px'], fn: 'translateX', totalDuration: '1s'});
-      const expected = [{step: 0, properties: [{property: 'transform', value: 'translateX(0)'}]},
-                      {step: 50, properties: [{property: 'transform', value: 'translateX(100px)'}]},
-                      {step: 100, properties: [{property: 'transform', value: 'translateX(150px)'}]}];
+
+    it('should return three keyframes for transform with translateY', function () {
+      const actual = process({ property: 'transform', value: ['0', '150px', '2'], fn: 'translateX', totalDuration: '1s' });
+      const expected = [{
+        step: 0,
+        properties: [{property: 'transform', value: 'translateX(0)'}]
+      }, {
+        step: 50,
+        properties: [{property: 'transform', value: 'translateX(100px)'}]
+      }, {
+        step: 100,
+        properties: [{property: 'transform', value: 'translateX(150px)'}]
+      }];
+
       expect(actual).to.deep.equal(expected);
     });
   });
